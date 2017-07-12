ee937c21fc3a291fe4287a7821ba9b6d93b2e7a4
pxai <pello_altadill@cuatrovientos.org>
2017-07-12 18:33:47 +0200

    First commit

---
+++ src/main/java/io/pello/cleancode/samples/solid/App.java
@@ -0,0 +1,13 @@
+package io.pello.cleancode.samples.solid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
